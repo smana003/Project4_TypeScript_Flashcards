c28c4c4b173994dd06fcf989125535e488964c86
Saksham <smanandhar96@gmail.com>
2023-02-17 22:11:27 -0800

    Working on showing decks on landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,22 @@ config();
 
 import express, {Request, Response} from "express";
 import mongoose from 'mongoose';
+import cors from 'cors';
 import Deck from './models/deck';
 
 const PORT = 5000;
 
 const app = express();
 
+app.use(cors({
+  origin: '*',
+}));
 app.use(express.json());
+
+app.get('/decks', async (req: Request, res: Response) => {
+  const decks = await Deck.find();
+  res.json(decks);
+});
 
 app.post("/decks", async (req: Request, res: Response) => {
   console.log(req.body);
